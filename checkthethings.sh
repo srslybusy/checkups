873456744e1b7db683977f795d7797cb996c6b65
srslybusy <31709882+srslybusy@users.noreply.github.com>
2017-09-06 14:08:01 -0700

    Create checkthethings.sh
    first commit, rough draft

---
+++ checkthethings.sh
@@ -0,0 +1,33 @@
+!#/bin/bash/
+
+## check if environment variable for checkthethings exists and if points to installed script matching this scripts hash, if not install
+
+
+
+## check your stuffs script, runs network and local system scans (rootkit scans etc) to ensure local assurance, whilst deploying network scans to determine environment
+
+
+
+## if everything is clear, validate keys for all the things, then do update, upgrade, aggregate and dsiplay past errors and warnings, possible intrusion attempts, etc
+
+
+
+## if not, go lock down, create environment of no trust, delete and recreate ssh keys(HOST AND CLIENT), refresh all tunneling, disable all unnecessary network resources
+
+
+
+## then execute choice.sh, else
+
+
+
+## in the clear, continue on checks, secure all network resources with the same level of scrutiny as above, triage by distance, hops and value
+
+
+
+## secure everything
+
+
+
+## produce a conky with relevant data streams
+
+
